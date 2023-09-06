9fa7a788a3d6648483b51bcc8753cd85b7f378c8
Cihat Acar <cihat.acar@amis.nl>
2023-09-06 13:59:17 +0200

    Comments added.

--- fileSystemModule.js
+++ fileSystemModule.js
@@ -1,8 +1,10 @@
 const fs = require("fs");
 
+// Synchronous method
 // const files = fs.readdirSync("./");
 // console.log(files);
 
+// Asynchronous method (preferred)
 fs.readdir("./", function(err, files) {
     if (err) console.log("Error", err);
     else console.log("Result", files);
