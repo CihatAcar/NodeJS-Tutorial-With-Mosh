bc4d6cdd2c56ed7b0373994c9017293295e31726
Cihat Acar <cihat.acar@amis.nl>
2023-09-06 13:27:48 +0200

    Notes added.

--- moduleWrapperFunction.js
+++ moduleWrapperFunction.js
@@ -1,3 +1,8 @@
+// node wraps the module as a function when running JavaScript code like in the following way:
+// So it collects exports, require, and module. The file name and directory name.
+// When we are executing the code node wraps all of this and runs the code so we are actually not seeing this.
+// So the following code is just a representation of the node module wrapper. 
+
 (function (exports, require, module, _filename, _dirname){
 
     console.log(__filename);
